00d7a83f4b81213388546a23a2206073d44762b9
J-45 <64985480+J-45@users.noreply.github.com>
2022-02-01 00:19:02 +0100

    Update code.js

--- code.js
+++ code.js
@@ -13,39 +13,59 @@ if ("WebSocket" in window) {
             'token':'69',
             'method':'system.multicall',
             'params': [[
-            // {'methodName':'system.listMethods',
-            // 'params':[['token:69']]},
+
+            {'methodName':'aria2.tellActive',
+            'params':['token:69']},
+            {'methodName':'aria2.tellWaiting',
+            'params':['token:69',0,666]},
+            {'methodName':'aria2.tellStopped',
+            'params':['token:69',0,666]},
 
             // {'methodName':'system.listNotifications',
             // 'params':[['token:69']]},
-            
-            {'methodName':'aria2.getVersion',
-            'params':['token:69']},
+
+            // {'methodName':'aria2.getVersion',
+            // 'params':['token:69']},
 
             // {'methodName':'aria2.getGlobalOption',
             // 'params':['token:69']},
 
-            // {'methodName':'aria2.tellActive',
-            // 'params':['token:69']},
-            // {'methodName':'aria2.tellWaiting',
-            // 'params':['token:69',0,666]},
-            // {'methodName':'aria2.tellStopped',
-            // 'params':['token:69',0,666]},
+            {'methodName':'aria2.getPeers',
+            'params':['token:69','40fc321f2f90ed96']},
+
         ]]
         }));
+    }
 
     ws.onmessage = function(event) {
         // prints [Object object] string and not the object
-        alert(`[message] Data received from server: ${event.data}`);
+        const rpc_respond = JSON.parse(event.data);
+        console.log(`[message] Data received from server: ${event.data}`);
+        // alert(rpc_respond.result);
+        document.getElementById("0").innerHTML = JSON.stringify(rpc_respond.result[0]);
+        // document.getElementById("0").innerHTML = JSON.stringify(rpc_respond.result[0][0][0].infoHash) + " - " +  JSON.stringify(rpc_respond.result[0][0][0].bittorrent.info.name);
+        document.getElementById("1").innerHTML = JSON.stringify(rpc_respond.result[1]);
+        document.getElementById("2").innerHTML = JSON.stringify(rpc_respond.result[2]);
+        document.getElementById("3").innerHTML = JSON.stringify(rpc_respond.result[3][0]);
+        
     };
 
-    ws.onclose = function() {   
-        // websocket is closed.
-        console.log("Connection is closed..."); 
+    ws.onclose = function(event) {
+        if (event.wasClean) {
+            console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+        } else {
+            // e.g. server process killed or network down
+            // event.code is usually 1006 in this case
+            console.log('[close] Connection died');
+        }
     };
-    }
+
+    ws.onerror = function(error) {
+        alert(`[error] ${error.message}`);
+    };
+
 }
 else {
     // The browser doesn't support WebSocket
     alert("WebSocket is NOT supported by your Web browser!");
- }
+}
