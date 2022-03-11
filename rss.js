be84cf7af2a4dba555468b22b63b6f750c7fa7cf
J-45 <perso@j45.eu>
2022-03-11 23:54:39 +0100

    ok

---
+++ rss.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const url = require('url');
+
+http
+    .createServer(function(req, res) {
+        const queryObject = url.parse(req.url, true).query;
+        console.log(queryObject);
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('Twitter account: ' + queryObject.from);
+    })
+    .listen(8080);
