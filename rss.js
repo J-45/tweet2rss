3d26cea600c4f21fc74fa7767f3ab6937f70b7de
J-45 <perso@j45.eu>
2022-03-13 08:52:36 +0100

    working

--- rss.js
+++ rss.js
@@ -6,30 +6,41 @@ puppeteer.use(StealthPlugin());
 var now = "";
 
 http.createServer(async function(req, res) {
+        const browser = await puppeteer.launch({ defaultViewport: null });
+        const page = await browser.newPage();
 
         const queryObject = url.parse(req.url, true).query;
         let twitter_account = queryObject.from;
-        console.log(queryObject);
         result = await tweet2rss(twitter_account);
         let now = new Date();
-
-        let rss = "";
+        console.log(now, queryObject);
+        let rss = `<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+<channel>
+<title>${twitter_account}'s tweets</title>
+<description>Twitter@${twitter_account}</description>
+<link>https://twitter.com/${twitter_account}</link>
+<atom:link href="http://217.160.13.95:8080/?from=${twitter_account}" rel="self" type="application/rss+xml"></atom:link>\n\n`;
         for (let index = 0; index < result.length; ++index) {
             let post = result[index];
-            rss += `>>>${post}<<<\n\n`;
+            rss += `<item>
+    <title>${post.slice(0, 100)}</title>
+    <description>${post}</description>
+<guid>http://217.160.13.95:8080/?from=${twitter_account}_${post.length}</guid>
+</item>`;
         }
-        rss += `\n\n${now.toLocaleString("fr-FR")} \nTwitter account: ${twitter_account} -  ${result.length} tweets`;
-
+        rss += `\n\n</channel>
+</rss>`;
+        console.log(`${now.toLocaleString("fr-FR")} == Twitter account: ${twitter_account} - ${result.length} tweets`);
         res.end(rss);
     })
     .listen(8080);
 
 // http://127.0.0.1:8080/?from=playlostark
 
-async function tweet2rss(twitter_account) {
+async function tweet2rss(page, twitter_account) {
     let tweets = [];
-    const browser = await puppeteer.launch({ defaultViewport: null });
-    const page = await browser.newPage();
+
     await page.setExtraHTTPHeaders({
         'accept-language': 'fr-FR,cs;q=0.8'
     });
@@ -42,16 +53,13 @@ async function tweet2rss(twitter_account) {
         waitUntil: 'networkidle2',
     });
 
+
     let html = await page.evaluate(() => { return document.documentElement.outerHTML; });
     const regexp = /id="id__\w+"><span class="css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0">([^<]+)<\/span>/gm;
-    // console.log(html.toString('utf8'));
-    // return html.toString('utf8');
-    if (html !== undefined) {
-        // html = html.split(`<span class="css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0">J'aime</span>`)[1];
-        // html = html.split('</header>')[0];
-        // result = html.split('data-testid="tweet">');
-        result = [...html.matchAll(regexp)];
 
+
+    if (html !== undefined) {
+        result = [...html.matchAll(regexp)];
         for (let index = 0; index < result.length; ++index) {
             let post = result[index][1].trim()
             console.log(`>>>${post}<<<`);
