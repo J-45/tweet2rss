425e5d01b53808e99f10483fb999abb80c6930f8
J-45 <perso@j45.eu>
2022-03-13 11:08:55 +0100

    1 chromium instance max

--- rss.js
+++ rss.js
@@ -3,15 +3,45 @@ const url = require('url');
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
-var now = "";
+var browser = null;
+var page = null;
 
-http.createServer(async function(req, res) {
-        const browser = await puppeteer.launch({ defaultViewport: null });
-        const page = await browser.newPage();
+(async() => {
 
+    browser = await puppeteer.launch({ defaultViewport: null });
+    [page] = await browser.pages();
+
+
+    http.createServer(async function(req, res) {
         const queryObject = url.parse(req.url, true).query;
         let twitter_account = queryObject.from;
-        result = await tweet2rss(twitter_account);
+        let tweets = [];
+
+        await page.setExtraHTTPHeaders({
+            'accept-language': 'fr-FR,cs;q=0.8'
+        });
+        await page.setCacheEnabled(false);
+        let width = 1080;
+        let height = 1920;
+        await page.setViewport({ width, height });
+        await page.goto('https://twitter.com/' + twitter_account, {
+            headless: true,
+            waitUntil: 'networkidle2',
+        });
+
+        let html = await page.evaluate(() => { return document.documentElement.outerHTML; });
+        const regexp = /id="id__\w+"><span class="css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0">([^<]+)<\/span>/gm;
+
+        if (html !== undefined) {
+            result = [...html.matchAll(regexp)];
+            for (let index = 0; index < result.length; ++index) {
+                let post = result[index][1].trim()
+                console.log(`>>>${post}<<<`);
+                tweets.push(post);
+            }
+        }
+        await page.screenshot({ path: 'tweets.png' });
+
         let now = new Date();
         console.log(now, queryObject);
         let rss = `<?xml version="1.0" encoding="UTF-8" ?>
@@ -20,55 +50,21 @@ http.createServer(async function(req, res) {
 <title>${twitter_account}'s tweets</title>
 <description>Twitter@${twitter_account}</description>
 <link>https://twitter.com/${twitter_account}</link>
-<atom:link href="http://217.160.13.95:8080/?from=${twitter_account}" rel="self" type="application/rss+xml"></atom:link>\n\n`;
-        for (let index = 0; index < result.length; ++index) {
-            let post = result[index];
+<atom:link href="http://127.0.0.1:8080/?from=${twitter_account}" rel="self" type="application/rss+xml"></atom:link>\n\n`;
+        for (let index = 0; index < tweets.length; ++index) {
+            let post = tweets[index];
             rss += `<item>
-    <title>${post.slice(0, 100)}</title>
-    <description>${post}</description>
-<guid>http://217.160.13.95:8080/?from=${twitter_account}_${post.length}</guid>
+<title>${post.slice(0, 100)}</title>
+<description>${post}</description>
+<guid>http://127.0.0.1:8080/?from=${twitter_account}_${post.length}</guid>
 </item>`;
         }
         rss += `\n\n</channel>
 </rss>`;
         console.log(`${now.toLocaleString("fr-FR")} == Twitter account: ${twitter_account} - ${result.length} tweets`);
         res.end(rss);
-    })
-    .listen(8080);
-
-// http://127.0.0.1:8080/?from=playlostark
-
-async function tweet2rss(page, twitter_account) {
-    let tweets = [];
-
-    await page.setExtraHTTPHeaders({
-        'accept-language': 'fr-FR,cs;q=0.8'
-    });
-    await page.setCacheEnabled(false);
-    let width = 1080;
-    let height = 1920;
-    await page.setViewport({ width, height });
-    await page.goto('https://twitter.com/' + twitter_account, {
-        headless: true,
-        waitUntil: 'networkidle2',
-    });
+    }).listen(8080);
 
 
-    let html = await page.evaluate(() => { return document.documentElement.outerHTML; });
-    const regexp = /id="id__\w+"><span class="css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0">([^<]+)<\/span>/gm;
-
-
-    if (html !== undefined) {
-        result = [...html.matchAll(regexp)];
-        for (let index = 0; index < result.length; ++index) {
-            let post = result[index][1].trim()
-            console.log(`>>>${post}<<<`);
-            tweets.push(post);
-        }
-    }
-
-
-    await page.screenshot({ path: 'tweets.png' });
-    await browser.close();
-    return tweets;
-}
+    // await browser.close();
+})();
