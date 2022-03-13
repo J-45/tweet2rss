4937af13deb331b35c0ddc0a07de2962d82fab7b
J-45 <perso@j45.eu>
2022-03-13 11:11:52 +0100

    ok

--- rss.js
+++ rss.js
@@ -51,19 +51,11 @@ var page = null;
 <description>Twitter@${twitter_account}</description>
 <link>https://twitter.com/${twitter_account}</link>
 <atom:link href="http://127.0.0.1:8080/?from=${twitter_account}" rel="self" type="application/rss+xml"></atom:link>\n\n`;
-<<<<<<< HEAD
         for (let index = 0; index < tweets.length; ++index) {
             let post = tweets[index];
             rss += `<item>
 <title>${post.slice(0, 100)}</title>
 <description>${post}</description>
-=======
-        for (let index = 0; index < result.length; ++index) {
-            let post = result[index];
-            rss += `<item>
-    <title>${post.slice(0, 100)}</title>
-    <description>${post}</description>
->>>>>>> cedb82188ef43e9b8863ef2979d5a53cff2e4778
 <guid>http://127.0.0.1:8080/?from=${twitter_account}_${post.length}</guid>
 </item>`;
         }
@@ -71,48 +63,8 @@ var page = null;
 </rss>`;
         console.log(`${now.toLocaleString("fr-FR")} == Twitter account: ${twitter_account} - ${result.length} tweets`);
         res.end(rss);
-<<<<<<< HEAD
     }).listen(8080);
 
 
     // await browser.close();
 })();
-=======
-    })
-    .listen(8080);
-
-async function tweet2rss(page, twitter_account) {
-    let tweets = [];
-
-    await page.setExtraHTTPHeaders({
-        'accept-language': 'fr-FR,cs;q=0.8'
-    });
-    await page.setCacheEnabled(false);
-    let width = 1080;
-    let height = 1920;
-    await page.setViewport({ width, height });
-    await page.goto('https://twitter.com/' + twitter_account, {
-        headless: true,
-        waitUntil: 'networkidle2',
-    });
-
-
-    let html = await page.evaluate(() => { return document.documentElement.outerHTML; });
-    const regexp = /id="id__\w+"><span class="css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0">([^<]+)<\/span>/gm;
-
-
-    if (html !== undefined) {
-        result = [...html.matchAll(regexp)];
-        for (let index = 0; index < result.length; ++index) {
-            let post = result[index][1].trim()
-            console.log(`>>>${post}<<<`);
-            tweets.push(post);
-        }
-    }
-
-
-    await page.screenshot({ path: 'tweets.png' });
-    await browser.close();
-    return tweets;
-}
->>>>>>> cedb82188ef43e9b8863ef2979d5a53cff2e4778
